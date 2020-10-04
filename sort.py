daa8846df4c5f34f6e9825adf376b8c5c9e8dc1b
Benedictus Kent <benedictuskent123@gmail.com>
2020-10-04 15:15:17 +0800

    debug and automate merging

--- sort.py
+++ sort.py
@@ -1,29 +1,31 @@
 import os
+import time
 import shutil
 
-def split_file(inputfile, file_size):
+def split_file(inputfile):
     count = 0                                                           # counter
     name = 0                                                            # file namer
     temp_data = []                                                      # hold data temporarily
 
-    if(os.path.isdir('text_files')):                                    # if directory exist, delete it
-        shutil.rmtree('text_files')
+    if(os.path.isdir('text2_files')):                                    # if directory exist, delete it
+        shutil.rmtree('text2_files')
 
-    os.mkdir('text_files/')                                             # make directory for the sake of tidiness
+    os.mkdir('text2_files/')                                             # make directory for the sake of tidiness
     
     # split files
     for i in inputfile:
         temp_data.append(int(i))
         count += 1
-        if(count == int(file_size / 100)):
-            outfile = open('file0' + str(name) + '.txt', 'w')           
+        if(count == 10):
+            outfile = open('file' + str(name) + '.txt', 'w')           
             for j in range(len(temp_data)):
                 outfile.write(str(temp_data[j]))
                 if(j < len(temp_data)-1):
                     outfile.write("\n")
             temp_data.clear()
             outfile.close()
-            shutil.move('file0' + str(name) + '.txt', 'text_files/')    # move text file to the directory
+            shutil.move('file' + str(name) + '.txt', 'text2_files/')    # move text file to the directory
+            print("file" + str(name) + ".txt splitting complete")
             count = 0
             name += 1
 
@@ -58,17 +60,17 @@ def sortarray(array):
             j += 1
             k += 1
 
-def sort_allfiles(file_size):
+def sort_allfiles(number_of_files):
     hold = []
-    for i in range(int(file_size/100)):
-        infile = open('text_files/file0' + str(i) + '.txt', 'r')
+    for i in range(number_of_files):
+        infile = open('text2_files/file' + str(i) + '.txt', 'r')
         for j in infile:
             hold.append(int(j))
         infile.close()
 
-        os.remove('text_files/file0' + str(i) + '.txt')
+        os.remove('text2_files/file' + str(i) + '.txt')
 
-        outfile = open('text_files/file0' + str(i) + '.txt', 'w')
+        outfile = open('text2_files/file' + str(i) + '.txt', 'w')
         sortarray(hold)
         for j in range(len(hold)):
             outfile.write(str(hold[j]))
@@ -76,12 +78,13 @@ def sort_allfiles(file_size):
                 outfile.write("\n")
         hold.clear()
         outfile.close()
+        print("file" + str(i) + ".txt done sorting")
 
 def merge(file1, file2, name):
     text1 = file1.readline()
     text2 = file2.readline()
 
-    outfile = open('text_files/file' + str(name) + '.txt', 'w')
+    outfile = open('text2_files/file' + str(name) + '.txt', 'w')
 
     while True:
         if(text1 != '' and text2 != ''):
@@ -103,68 +106,57 @@ def merge(file1, file2, name):
 
     outfile.close()
 
-infile = open("test.txt", 'r')
-file_size = os.stat("test.txt").st_size                                 # file size
-
-split_file(infile, file_size)
-sort_allfiles(file_size)
-
-# first merge
-name = 10
-for i in range(9):
-    if(i%2 == 0):
-        file1 = open('text_files/file0' + str(i) + '.txt', 'r')
-        file2 = open('text_files/file0' + str(i+1) + '.txt', 'r')
-        merge(file1, file2, name)
-        name += 1
-        file1.close()
-        file2.close()
-        os.remove('text_files/file0' + str(i) + '.txt')
-        os.remove('text_files/file0' + str(i+1) + '.txt')
-
-# second merge
-name = 15
-for i in range(4):
-    if(i%2 == 0):
-        if(os.path.isfile('text_files/file1' + str(i) + '.txt') and os.path.isfile('text_files/file1' + str(i+1) + '.txt')):
-            file1 = open('text_files/file1' + str(i) + '.txt', 'r')
-            file2 = open('text_files/file1' + str(i+1) + '.txt', 'r')
+def iteration(start, end, name):
+    for i in range(start, end):
+        if(i%2 == 0):
+            file1 = open('text2_files/file' + str(i) + '.txt', 'r')
+            file2 = open('text2_files/file' + str(i+1) + '.txt', 'r')
             merge(file1, file2, name)
+            print("file" + str(i) + ".txt and file" + str(i+1) + ".txt merge complete")
             name += 1
             file1.close()
             file2.close()
-            os.remove('text_files/file1' + str(i) + '.txt')
-            os.remove('text_files/file1' + str(i+1) + '.txt')
+            os.remove('text2_files/file' + str(i) + '.txt')
+            os.remove('text2_files/file' + str(i+1) + '.txt')
 
-# third merge
-name = 17
-file1 = open('text_files/file14.txt', 'r')
-file2 = open('text_files/file15.txt', 'r')
-merge(file1, file2, name)
-name += 1
-file1.close()
-file2.close()
-os.remove('text_files/file14.txt')
-os.remove('text_files/file15.txt')
 
-# last merge
-name = 18
-file1 = open('text_files/file16.txt', 'r')
-file2 = open('text_files/file17.txt', 'r')
-merge(file1, file2, name)
-name += 1
-file1.close()
-file2.close()
-os.remove('text_files/file16.txt')
-os.remove('text_files/file17.txt')
+infile = open("try.txt", 'r')
+file_size = os.stat("try.txt").st_size                                 # file size
+number_of_files = int(1000/10)
 
-'''
-testing = open('text_files/file18.txt', 'r')
-count = 0
-for i in testing:
-    if(int(i) == hold[i]):
-        count += 1
-print(count)
-testing.close()
-'''
+# split and sort
+start_timer = time.time()                                               # start time
+split_file(infile)                                                      # split files
+sort_allfiles(number_of_files)                                          # sort splitted files
+end_timer = time.time()                                                 # end time
+
+print("time to split and sort: " + str(end_timer-start_timer) + " seconds / " 
+        + str((end_timer-start_timer)/60) + " minutes")
+
+# merging automated
+start = 0
+end = number_of_files
+temp = end
+name = number_of_files
+
+start_timer = time.time()
+
+while temp >= 1:
+    iteration(start, end-1, name)
+    if(end%2 == 0):
+        start = end
+    else:
+        start = end-1
+    temp = int(temp/2)
+    end += temp
+    name = end
+
+if(len(os.listdir('text2_files/')) == 2):
+    iteration(start, end, name+1)
+
+end_timer = time.time()
+
+print("time to merge: " + str(end_timer-start_timer) + " seconds / " 
+        + str((end_timer-start_timer)/60) + " minutes")
+
 infile.close()
